0608a7ab414368dd547d54c48d4870e94620e84d
Shunsuke Sato <shunsuke.sato26@gmail.com>
2019-04-17 12:18:07 +0200

    Switched to velocity gauge from length gauge.

--- main.f90
+++ main.f90
@@ -1,8 +1,19 @@
+include "parallel.f90"
+include "communication.f90"
+
+
 module global_variables
+  use mpi
+  use parallel
+  use communication
   implicit none
 ! mathematical parameters
   real(8),parameter :: pi = 4d0*atan(1d0)
   complex(8),parameter :: zi = (0d0, 1d0)
+
+! physics parameters
+  real(8),parameter :: ev = 1d0/27.2114d0
+  real(8),parameter :: fs = 1d0/0.024189d0
 
 ! material parameters
   real(8),parameter :: v_fermi = 1d0
@@ -10,12 +21,12 @@ module global_variables
   real(8),parameter :: kx0_K=0d0,ky0_K=0d0
 
 ! system
-  integer :: nk,nkz
-  real(8) :: rho_11
-  real(8),allocatable :: rho_22(:,:),rho_33(:,:)
-  complex(8),allocatable :: zrho_12(:,:), zrho_13(:,:), zrho_23(:,:)
-  real(8),allocatable :: eps_v(:), eps_c(:), eps_k(:,:)
-  complex(8),allocatable :: zdip_v(:,:), zdip_c(:,:), zdip_vc(:,:,:)
+  integer :: nkxy,nkz,nk
+  integer :: nk_start, nk_end
+  integer :: nk_average, nk_remainder
+  complex(8),allocatable :: zrho_k(:,:,:)
+  real(8),allocatable :: eps_k(:)
+  complex(8),allocatable :: zdip_B(:), zdip_A(:)
   real(8),allocatable :: kx0(:),ky0(:),kz0(:)
   real(8).allocatable :: kx(:),ky(:),kz(:)
 
@@ -26,7 +37,6 @@ module global_variables
 
 ! laser
   real(8),allocatable :: At_IR(:,:), At_IR_dt2(:,:)
-  real(8),allocatable :: Et_IR(:,:), Et_IR_dt2(:,:)
   real(8),allocatable :: Et_XUV(:), Et_XUV_dt2(:)
 
 
@@ -37,10 +47,16 @@ program main
   use global_variables
   implicit none
 
+
+  call init_parallel
+
   call input
   call initialize
 
   call time_propagation
+
+
+  call fin_parallel
 
 end program main
 !-------------------------------------------------------------------------------
@@ -48,8 +64,14 @@ subroutine input
   use global_variables
   implicit none
 
-  nk = 128
-  nkz = 64
+  nkxy = 64
+  nkz  = 64
+  nk = nkxy*nkz
+
+  Tprop = 40d0*fs
+  dt = 0.01d0
+  nt = aint(Tprop/dt)+1
+
 
 
 end subroutine input
@@ -58,79 +80,37 @@ subroutine initialize
   use global_variables
   implicit none
 
-  rho_11 = 1d0
-  allocate(rho_22(nk,nkz), rho_33(nk,nkz))
-  rho_22 = 0d0; rho_33 = 0d0
-  allocate(zrho_12(nk,nkz), zrho_13(nk,nkz), zrho_23(nk,nkz))
-  zrho_12 = 0d0; zrho_12 = 0d0; zrho_23 = 0d0
-  allocate(eps_v(nk), eps_c(nk), eps_k(nk,nkz) )
-  allocate(zdip_v(nk,nkz), zdip_c(nk,nkz), zdip_vc(2,nk,nkz))
+  if(nk < comm_nproc_global) call error_finalize('Error: nk < # of MPI processes')
+  nk_average = nk/comm_nproc_global
+  nk_remainder = mod(nk,comm_nproc_global)
+  if(comm_id_global+1 <= nk_remainder)then
+    nk_start = 1 + comm_id_global*(nk_average+1)
+    nk_end   = nk_start + (nk_average + 1) -1
+  else
+    nk_start = 1 + nk_remainder*(nk_average+1) + nk_average*(comm_id_global - nk_remainder)
+    nk_end    = nk_start + nk_average  -1
+  end if
+  
+
+  allocate(zrho_k(3,3,nk_start:nk_end))
+  allocate(zdip_v(nk), zdip_c(nk))
   allocate(kx0(nk),ky0(nk),kz0(nkz))
   allocate(kx(nk),ky(nk),kz(nkz))
 
+  zrho_k = 0d0
+  zrho_k(1,1,:) = 1d0
 
-  kxy0_v = 0d0 ! has to be K or K' point.
+  call init_k_grids
+  call init_laser
 
 
 
 end subroutine initialize
 !-------------------------------------------------------------------------------
-subroutine drho_dt(rho_22_t, rho_33_t, zrho_12_t, zrho_13_t, zrho_23_t, &
-  drho_22_t, drho_33_t, dzrho_12_t, dzrho_13_t, dzrho_23_t, EXUV_t, EIR_t)
-  use global_variables
-  implicit none
-  real(8),intent(in) :: rho_22_t(nk,nkz),rho_33_t(nk,nkz)
-  complex(8),intent(in) :: zrho_12_t(nk,nkz), zrho_13_t(nk,nkz), zrho_23_t(nk,nkz)
-  real(8),intent(out) :: drho_22_t(nk,nkz), drho_33_t(nk,nkz)
-  complex(8),intent(out) :: dzrho_12_t(nk,nkz),dzrho_13_t(nk,nkz),dzrho_23_t(nk,nkz)
-  real(8),intent(in) :: EXU_t, EIR_t(2)
-
-  integer :: ik, ikz
-
-  do ik = 1, nk
-    do ikz = 1, nkz
-
-      drho_22_t(ik,ikz) = 2d0*real(-zi*(&
-        EXUV_t*conjg(zdip_v(ik,ikz))*zrho_12_t(ik,ikz) &
-       +(EIR_t(1)*zdip_vc(1,ik,ikz)+EIR_t(2)*zdip_vc(2,ik,ikz))&
-       *conjg(zrho_23_t(ik,ikz))&
-        ))
-
-      drho_33_t(ik,ikz) = 2d0*real(-zi*(&
-        EXUV_t*conjg(zdip_c(ik,ikz))*zrho_13_t(ik,ikz) &
-       +conjg(EIR_t(1)*zdip_vc(1,ik,ikz)+EIR_t(2)*zdip_vc(2,ik,ikz))&
-       *zrho_23_t(ik,ikz)&
-       ))
-
-      dzrho_12_t(ik,ikz) = zi*(eps_k(ik,ikz)-eps_v(ik))*zrho_12_t(ik,ikz) &
-        -zi*EXUV_t*zdip_v(ik,ikz)* 1d0 &
-        -zi*EXUV_t*zdip_c(ik,ikz)*conjg(zrho_23_t(ik,ikz)) &
-        +zi*conjg(EIR_t(1)*zdip_vc(1,ik,ikz)+EIR_t(2)*zdip_vc(2,ik,ikz))&
-        *zrho_13_t(ik,ikz) -zrho_12_t(ik,ikz)/T12
-
-      dzrho_13_t(ik,ikz) = zi*(eps_k(ik,ikz)-eps_c(ik))*zrho_13_t(ik,ikz) &
-        -zi*EXUV_t*zdip_c(ik,ikz)* 1d0 &
-        -zi*EXUV_t*zdip_v(ik,ikz)*zrho_23_t(ik,ikz) &
-        +zi*(EIR_t(1)*zdip_vc(1,ik,ikz)+EIR_t(2)*zdip_vc(2,ik,ikz))&
-        *zrho_12_t(ik,ikz) -zrho_13_t(ik,ikz)/T13
-
-      dzrho_23_t(ik,ikz) = -zi*(eps_c(ik)-eps_v(ik))*zrho_23_t(ik,ikz) &
-        -zi*(EIR_t(1)*zdip_vc(1,ik,ikz)+EIR_t(2)*zdip_vc(2,ik,ikz)) &
-        *(rho_33_t(ik,ikz)-rho_22_t(ik,ikz)) &
-        -zi*EXUV_t*conjg(zdip_v(ik,ikz))*zrho_13_t(ik,ikz) &
-        +zi*EXUV_t*zdip_c(ik,ikz)*conjg(zrho_12_t(ik,ikz)) -zrho_23_t(ik,ikz)/T23
-
-    end do
-  end do
-
-end subroutine drho_dt
-!-------------------------------------------------------------------------------
 subroutine time_propagation
   use global_variables
   implicit none
   integer :: it
-
-  call init_laser
 
   do it = 0, nt
 
@@ -145,43 +125,121 @@ end subroutine time_propagation
 subroutine dt_evolve(it)
   use global_variables
   implicit none
-  real(8) :: At_IR_t(2), Et_IR_t(2), Et_XUV_t
+  integer,intent(in) :: it
 
-! time, t
-  At_IR_t(:) = At_IR(:,it)
-  Et_IR_t(:) = Et_IR(:,it)
-  Et_XUV_t = Et_XUV(it)
-
-  call set_matrix_elements(At_IR_t)
-
+  do ik = nk_start, nk_end
+    call dt_evolve_k(it,ik)
+  end do
 
 
 end subroutine dt_evolve
 !-------------------------------------------------------------------------------
-subroutine set_matrix_elements(At_IR_t)
+subroutine dt_evolve_k(it,ik)
   use global_variables
   implicit none
-  real(8),intent(in) :: At_IR_t(2)
-  integer :: ik, ikz
+  integer,intent(in) :: it, ik
+  real(8) :: kxt, kyt, kzt, eps_k
+  real(8) :: At_IR_t(2), Et_XUV_t
+  complex(8) :: zrho_t(3,3),zham(3,3), zLrho_rk(3,3,4)
 
-  kx(:) = kx0(:) + At_IR_t(1)
-  ky(:) = ky0(:) + At_IR_t(2)
-  kz = kz0
+! time, t
+  At_IR_t(:) = At_IR(:,it)
+  Et_XUV_t = Et_XUV(it)
 
-  do ik = 1, nk
-    eps_c(ik) = v_fermi*sqrt(kx(ik)**2+ky(ik)**2)
-    eps_v(ik) = -eps_c(ik)
-  end do
+  kxt = kx0(ik) + At_IR_t(1)
+  kyt = ky0(ik) + At_IR_t(2)
+  kzt = kz0(ik)
 
-  do ik = 1, nk
-    do ikz = 1, nkz
-      eps_k(ik,ikz) = 0.5d0*(&
-        (kx(ik)+kx0_K)**2+(ky(ik)+ky0_K)**2+kz(ikz)**2)
-    end do
-  end do
+  eps_k = 0.5d0*( (kxt+kx0_K)**2 + (kyt+ky0_K)**2 + kzt**2)
 
 
+  zham(1,1) = 0d0
+  zham(2,1) = zdip_B(ik)*Et_XUV_t
+  zham(3,1) = zdip_A(ik)*Et_XUV_t
+  zham(1,2) = conjg(zham(2,1))
+  zham(2,2) = eps_k
+  zham(3,2) = v_fermi*(kxt+zi*kyt)
+  zham(1,3) = conjg(zham(3,1))
+  zham(2,3) = conjg(zham(3,2))
+  zham(3,3) = eps_k
 
-end subroutine set_matrix_elements
+! RK1
+  zrho_t(:,:) = zrho_k(:,:,ik)
+  call zLrho_op(zrho_t,zham,zLrho_rk(:,:,1))
+
+
+! time, t+dt/2
+  At_IR_t(:) = At_IR_dt2(:,it)
+  Et_XUV_t = Et_XUV_dt2(it)
+
+  kxt = kx0(ik) + At_IR_t(1)
+  kyt = ky0(ik) + At_IR_t(2)
+  kzt = kz0(ik)
+
+  eps_k = 0.5d0*( (kxt+kx0_K)**2 + (kyt+ky0_K)**2 + kzt**2)
+
+
+  zham(1,1) = 0d0
+  zham(2,1) = zdip_B(ik)*Et_XUV_t
+  zham(3,1) = zdip_A(ik)*Et_XUV_t
+  zham(1,2) = conjg(zham(2,1))
+  zham(2,2) = eps_k
+  zham(3,2) = v_fermi*(kxt+zi*kyt)
+  zham(1,3) = conjg(zham(3,1))
+  zham(2,3) = conjg(zham(3,2))
+  zham(3,3) = eps_k
+
+
+! RK2
+  zrho_t(:,:) = zrho_k(:,:,ik) + 0.5d0*dt*zLrho_rk(:,:,1)
+  call zLrho_op(zrho_t,zham,zLrho_rk(:,:,2))
+
+! RK3
+  zrho_t(:,:) = zrho_k(:,:,ik) + 0.5d0*dt*zLrho_rk(:,:,2)
+  call zLrho_op(zrho_t,zham,zLrho_rk(:,:,3))
+
+! time, t+dt
+  At_IR_t(:) = At_IR(:,it+1)
+  Et_XUV_t = Et_XUV(it+1)
+
+  kxt = kx0(ik) + At_IR_t(1)
+  kyt = ky0(ik) + At_IR_t(2)
+  kzt = kz0(ik)
+
+  eps_k = 0.5d0*( (kxt+kx0_K)**2 + (kyt+ky0_K)**2 + kzt**2)
+
+
+  zham(1,1) = 0d0
+  zham(2,1) = zdip_B(ik)*Et_XUV_t
+  zham(3,1) = zdip_A(ik)*Et_XUV_t
+  zham(1,2) = conjg(zham(2,1))
+  zham(2,2) = eps_k
+  zham(3,2) = v_fermi*(kxt+zi*kyt)
+  zham(1,3) = conjg(zham(3,1))
+  zham(2,3) = conjg(zham(3,2))
+  zham(3,3) = eps_k
+
+! RK4
+  zrho_t(:,:) = zrho_k(:,:,ik) + dt*zLrho_rk(:,:,3)
+  call zLrho_op(zrho_t,zham,zLrho_rk(:,:,4))
+
+  
+  zrho_k(:,:,ik) + zrho_k(:,:,ik) + dt/6d0*(zLrho_rk(:,:,1)   &
+                                       +2d0*zLrho_rk(:,:,2)   &
+                                       +2d0*zLrho_rk(:,:,3)   &
+                                       +    zLrho_rk(:,:,4))
+
+
+end subroutine dt_evolve_k
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 !-------------------------------------------------------------------------------
 !-------------------------------------------------------------------------------
